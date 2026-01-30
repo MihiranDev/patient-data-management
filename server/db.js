9c862c55d02ce82a4b0c4a351ddda613c57122cd
shalinijayakumar16 <23cs1252@stjosephstechnology.ac.in>
2026-01-30 21:33:44 +0530

    Fix MYSQL Connection for cloud

--- server/db.js
+++ server/db.js
@@ -1,17 +1,19 @@
-
-require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'SHP#26#jku',
-    database: 'patient_management',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL');
+  if (err) {
+    console.error("❌ DB connection failed:", err);
+  } else {
+    console.log("✅ MySQL connected successfully");
+  }
 });
 
 module.exports = db;
